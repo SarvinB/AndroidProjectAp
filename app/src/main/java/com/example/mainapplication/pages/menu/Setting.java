eeb7c1f90958056f8133027402b13735b2e4d83f
SarvinB <87063732+SarvinB@users.noreply.github.com>
2021-07-13 12:23:26 +0430

    1

---
+++ app/src/main/java/com/example/mainapplication/pages/menu/Setting.java
@@ -0,0 +1,26 @@
+package com.example.mainapplication.pages.menu;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.mainapplication.R;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Setting extends Fragment
+{
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.setting_fragment, container, false);
+        return view;
+    }
+}
